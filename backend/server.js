ded18fef5d5b094538e2977c1f3eae96c23b5f2c
Prerana Nishad <prerananishad6@gmail.com>
2026-04-20 00:42:55 +0530

    Ready for Deploying

--- backend/server.js
+++ backend/server.js
@@ -5,22 +5,38 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 // import articlesRoute from "./routes/articles.js";
 
-
 dotenv.config();
 
 const app = express();
 
 /* Middleware */
-app.use(cors());
 app.use(express.json());
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://getkit.vercel.app"
+  ],
+  credentials: true
+}));
 
 /* MongoDB Connection */
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
+
+/* Test Route */
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
 
 /* Routes */
 app.use("/api/auth", authRoutes);
 // app.use("/api/articles", articlesRoute);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+/* Port */
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
